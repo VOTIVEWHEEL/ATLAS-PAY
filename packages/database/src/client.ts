716b5aa1ea3fd57ec8d6084a292230b18ac95d3a
VotiveWheel <163951580+VOTIVEWHEEL@users.noreply.github.com>
2026-06-03 13:21:41 +0100

    Create client.ts

---
+++ packages/database/src/client.ts
@@ -0,0 +1,10 @@
+import { Pool } from 'pg';
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+export const db = pool;
