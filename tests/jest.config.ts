be4a45e304a687c9cce185a1aaf919a4222064fb
VotiveWheel <163951580+VOTIVEWHEEL@users.noreply.github.com>
2026-06-03 13:50:25 +0100

    Create jest.config.ts

---
+++ tests/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '@atlaspay/shared': '<rootDir>/../packages/shared/src',
+  },
+};
+
+export default config;
